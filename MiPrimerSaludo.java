ad2c7010daea23dfa1176a8a59a58b5878fd0dc0
yesiddiaz1 <yesid.diazaar@cun.edu.co>
2025-08-12 12:46:49 -0500

    Add files via upload

---
+++ MiPrimerSaludo.java
@@ -0,0 +1,20 @@
+// Este programa imprime un saludo personalizado
+public class MiPrimerSaludo {
+    public static void main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("********");
+        System.out.println(" *****");
+        System.out.println("  ***");
+        System.out.println("   *");
+        
+        System.out.println("---------------------");
+        
+         System.out.println(" @@@@@@@");
+        System.out.println("@| O O |@");
+        System.out.println("(|  ^  |)");
+        System.out.println(" | [_] |");
+        System.out.println(" +-----+");
+    }
+}
